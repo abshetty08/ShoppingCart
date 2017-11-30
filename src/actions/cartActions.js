7e8a89fccfb1b7bfaa31f93da4084f3222ad80b0
Abhishek Shetty <abshetty08@gmail.com>
2017-11-30 09:23:31 -0500

    Added reducers and actions separately

---
+++ src/actions/cartActions.js
@@ -0,0 +1,9 @@
+// ADD to cart
+"use strict"
+// ADD to cart
+export function addToCart(book){
+	return {
+		type:"ADD_TO_CART",
+		payload: book
+	}
+}
---
+++ src/app.js
@@ -0,0 +1,51 @@
+"use strict"
+import {applyMiddleware, createStore} from 'redux';
+
+// IMPORT COMBINED REDUCERS
+import reducers from './reducers/index';
+
+// IMPORT ACTIONS
+import {addToCart} from './actions/cartActions';
+import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
+
+// STEP 1 create the store
+const store = createStore(reducers);
+
+store.subscribe(function(){
+	console.log('current state is: ', store.getState());
+	// console.log('current price: ', store.getState()[1].price);
+})
+
+// STEP 2 create and dispatch actions
+// POSTBOOK
+store.dispatch(postBooks(
+	[{
+		id: 1,
+		title:'this is the book title',
+		description: 'this is the book description',
+		price: 33.33
+	},
+	{
+		id: 2,
+		title:'this is the second book title',
+		description: 'this is the second book description',
+		price: 50
+	}]
+))
+
+// DELETE a book
+store.dispatch(deleteBooks(
+	{id: 1}
+))
+
+// UPDATE a book
+store.dispatch(updateBooks(
+	{
+		id: 2,
+		title:'Learn React in 24h'
+	}
+))
+
+//-->> CART ACTIONS <<--
+// ADD TO CART
+store.dispatch(addToCart([{id: 1}]))
