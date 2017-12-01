0e0a5d1a02b92db1e2bcbb87571e6622e4799e1d
Abhishek Shetty <abshetty08@gmail.com>
2017-11-30 19:34:16 -0500

    Add Navbar and footer

--- src/app.js
+++ src/app.js
@@ -21,10 +21,16 @@ const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 import BooksList from './components/pages/bookslist';
+import Menu from './components/menu';
+import Footer from './components/footer';
 
 render(
 	<Provider store={store}>
+	<div>
+		<Menu />
 		<BooksList />
+		<Footer />
+	</div>
 	</Provider>, document.getElementById('app')
 );
 
